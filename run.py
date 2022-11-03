dd73b29ed2826b6d9d9eaba8288975dfb22604dd
Vaibhav1612 <vaibhavkundaliya7@gmail.com>
2022-11-03 20:05:49 +0530

    updated host

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import os
 app = create_app()
 if __name__ == "__main__":
     app.secret_key = 'mysecret'
-    app.run(debug=False, host=os.getenv("HOST"), port=os.getenv("PORT"))
+    app.run(debug=False, host="hospitable-store-production.up.railway.app", port=os.getenv("PORT"))
