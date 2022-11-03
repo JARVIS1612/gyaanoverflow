b866d7e010ae3c27c7c1b3b55813a331ba5f0b06
Vaibhav1612 <vaibhavkundaliya7@gmail.com>
2022-11-03 19:53:31 +0530

    remove dotenv

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from GOF import create_app
 import os
-from dotenv import load_dotenv
+
 app = create_app()
-load_dotenv()
 if __name__ == "__main__":
     app.secret_key = 'mysecret'
     app.run(debug=False, host="0.0.0.0", port=os.getenv("PORT"))
