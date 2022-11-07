4bbfdb42c42d757e281ef73aa7c2bd5cc8121a3a
Vaibhav1612 <vaibhavkundaliya7@gmail.com>
2022-11-07 23:59:09 +0530

    nltk.download('omw-1.4')

--- GOF/SearchEngine.py
+++ GOF/SearchEngine.py
@@ -11,6 +11,8 @@ import nltk
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('stopwords')
+nltk.download('omw-1.4')
+
 def TextPreprocessing(text):
     if not text:
         return
